6864e10d32854b60ac8516b939d9bcbaa631d91b
DavidWilld <103283145+DavidCondoriAguilar@users.noreply.github.com>
2023-08-24 14:14:39 -0500

    “featReact”

--- src/common/MainFooter.jsx
+++ src/common/MainFooter.jsx
@@ -1,11 +1,27 @@
 import "./MainFooter.css";
 
-
 export default function MainFooter() {
   return (
     <footer id="main-footer">
       <div className="container  text-center py-3">
-        2023 Todos los derechos Reservados
+        <div className="row">
+          <div className="col-12">
+            <p>2023 Todos los derechos Reservados</p>
+          </div>
+          <div className="col-12">
+            <p>
+              Hecho con por{" "}
+              <a
+                className="text-decoration-none text-white"
+                href="https://davidcondoriaguilar.github.io/David-dev/"
+                target="_blank"
+              >
+                {" "}
+                ❤️ David
+              </a>
+            </p>
+          </div>
+        </div>
       </div>
     </footer>
   );
